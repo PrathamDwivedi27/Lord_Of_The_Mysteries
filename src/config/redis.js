74f386d55203d92ee23f5dd81f8aaa14b9f73f7d
Pratham-fursatAI <prathamd549@gmail.com>
2025-06-05 01:10:15 +0530

    [Fix]: Fixing small things to get it deployment ready

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,10 +1,10 @@
 import Redis from 'ioredis';
 import logger from '../utils/logger.js';
+import { REDIS_HOST, REDIS_PORT } from './server-config.js';
 
 const redisClient = new Redis({
-  host: 'localhost',
-  port: 6379,
-  // password, db, etc. if needed
+  host: REDIS_HOST,
+  port: REDIS_PORT,
 });
 
 redisClient.on('connect', () => logger.info('Connected to Redis'));
--- src/config/server-config.js
+++ src/config/server-config.js
@@ -5,3 +5,5 @@ dotenv.config();
 export const PORT=process.env.PORT;
 export const MONGODB_URI=process.env.MONGODB_URI;
 export const Admin_Password=process.env.Admin_Password;
+export const REDIS_HOST=process.env.REDIS_HOST;
+export const REDIS_PORT=process.env.REDIS_PORT;
