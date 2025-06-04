ef5451c424076c43b68d42eee6e015d360825058
Pratham-fursatAI <prathamd549@gmail.com>
2025-06-05 03:25:42 +0530

    [Fix]: fix the admin middleware

--- src/middleware/isAdmin.js
+++ src/middleware/isAdmin.js
@@ -4,5 +4,8 @@ export function isAdmin(req, res, next) {
   if (token === process.env.Admin_Password) {
     return next();
   }
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized: No token provided.' });
+  }
   return res.status(403).json({ message: 'Forbidden: Admins only.' });
 }
